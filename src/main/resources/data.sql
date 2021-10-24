30b67fa1b20f1acb5fc49923b4af62369e206d00
Hubert <hubert.pilka@outlook.com>
2021-10-24 21:34:33 +0200

    db load

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO employee (id, first_name, last_name, email, salary)
-VALUES (nextval('employee_sequence'), 'Piotr', 'Nowak', 'piotr.nowak@gmail.com', '3000'), 
-    (nextval('employee_sequence'), 'Jan', 'Kowalski', 'jan.kowalski@outlook.com', '5000'),
-    (nextval('employee_sequence'), 'Damian', 'Wójcik', 'd.woj@o2.pl', '3800'),
-    (nextval('employee_sequence'), 'Alan', 'Partyka', 'alan1998@gmail.com', '2800');
---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO employee (id, first_name, last_name, email, salary) VALUES (nextval('employee_sequence'), 'Piotr', 'Nowak', 'piotr.nowak@gmail.com', '3000');
+INSERT INTO employee (id, first_name, last_name, email, salary) VALUES (nextval('employee_sequence'), 'Jan', 'Kowalski', 'jan.kowalski@outlook.com', '5000');
+INSERT INTO employee (id, first_name, last_name, email, salary) VALUES (nextval('employee_sequence'), 'Damian', 'Wójcik', 'd.woj@o2.pl', '3800');
+INSERT INTO employee (id, first_name, last_name, email, salary) VALUES (nextval('employee_sequence'), 'Alan', 'Partyka', 'alan1998@gmail.com', '2800');
