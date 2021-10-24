758c0a4f9ce982bc3a08ec0e6ea1cfe68695f5ea
Bartosz Kabała <59928515+BarKabal@users.noreply.github.com>
2021-10-25 00:23:00 +0200

    dodane testy add/get

--- src/test/java/com/example/employeesmanager/employee/EmployeeServiceTest.java
+++ src/test/java/com/example/employeesmanager/employee/EmployeeServiceTest.java
@@ -8,7 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 @DisplayName("Tests for the database")
 @ExtendWith(MockitoExtension.class)
@@ -38,7 +42,7 @@ class EmployeeServiceTest {
         void updateTestInit() {
             initialEmail = "jsmith@example.com";
             initialEmployee = new Employee("John", "Smith", initialEmail, 5000);
-            changedEmployee = new Employee("John", "Smith",initialEmail, 10000);
+            changedEmployee = new Employee("John", "Smith", initialEmail, 10000);
             employeeService.addEmployee(initialEmployee);
         }
 
@@ -46,13 +50,13 @@ class EmployeeServiceTest {
         void updateTest() {
             employeeService.updateEmployee(changedEmployee);
 
-
         }
 
         @AfterEach
         void updateTestTerminate() {
 
         }
+
         @Nested
         class UpdateTestWhenX {
 
@@ -60,22 +64,67 @@ class EmployeeServiceTest {
     }
 
     @Nested
+    @DisplayName("Tests for removing from the database")
+    class RemoveTests {
+
+    }
+
+    @Nested
+    @DisplayName("Tests for add command")
+    class AddTests {
+
+        @Test
+        @DisplayName("Proper use of add command")
+        void proper_add_command_test() {
+            Employee John = new Employee(1L, "John", "Kowalski", "jonkowalski@gmail.com", 3000);
+            employeeService.addEmployee(John);
+            assertThat(employeeService.getEmployee(1L)).isEqualTo(John);
+        }
+
+        @Test
+        @DisplayName("Improper use of add command - duplicates")
+        void get_command_with_improper_data() {
+            assertThatThrownBy(() -> {
+                Employee John = new Employee(1L, "John", "Kowalski", "jonkowalski@gmail.com", 3000);
+                employeeService.addEmployee(John);
+                employeeService.addEmployee(John);
+            }).isInstanceOf(IllegalStateException.class).hasMessageContaining("Employee already exists");
+        }
+
+    }
+
+    @Nested
     @DisplayName("Tests for get command")
-    class GetTests{
+    class GetTests {
 
         Employee John, Paul, Candice;
 
         @BeforeEach
         void updateTestInit() {
             John = new Employee("John", "Smith", "smith@gmail.com", 5000);
-            employeeService.addEmployee(John);
             employeeService.addEmployee(new Employee("Paul", "Smith", "paul@gmail.com", 9000));
             employeeService.addEmployee(new Employee("Candice", "Smith", "candice@gmail.com", 9000));
         }
+
         @Test
-        void getTest(){
+        @DisplayName("Proper use of get command")
+        void proper_get_command_test() {
             Employee gottenEmployee = employeeService.getEmployee(1L);
             assertThat(gottenEmployee).isEqualTo(John);
+        }
+
+        @Test
+        @DisplayName("Improper use of get command - wrong id number")
+        void get_command_with_improper_data() {
+            assertThatThrownBy(() -> {
+                Employee gottenEmployee = employeeService.getEmployee(4L);
+            }).isInstanceOf(IllegalStateException.class).hasMessageContaining("4");
+        }
+
+        @Test
+        void get_all_command() {
+            List<Employee> gottenList = employeeService.getAllEmployees();
+            assertThat(gottenList.size()).isEqualTo(3);
         }
     }
 }
